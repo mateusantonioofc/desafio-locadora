8987e59f2236140779c20d35b0d80e6b0ca78c90
Mateus <mateusantonioofc@gmail.com>
2025-01-14 19:25:54 -0300

    pre-build

---
+++ src/main/java/org/locadora/RegrasSistenaLocadora.java
@@ -0,0 +1,10 @@
+package org.locadora;
+
+// metodos que precisam ser adicionados
+public interface RegrasSistenaLocadora {
+
+    void alugarVeiculo();
+    void devolverVeiculo();
+    void buscarCarro();
+    
+}
