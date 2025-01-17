d7204af0c607e3f5f2f2fa30e326df472483c8e1
Mateus <mateusantonioofc@gmail.com>
2025-01-17 00:06:07 -0300

    create search methods

--- src/main/java/org/locadora/sistema/SistemaLocadora.java
+++ src/main/java/org/locadora/sistema/SistemaLocadora.java
@@ -2,19 +2,37 @@ package org.locadora.sistema;
 
 import java.util.ArrayList;
 import java.util.List;
-import org.locadora.veiculos.VeiculoModel;
+import org.locadora.veiculos.Veiculo;
 
 public class SistemaLocadora {
     
-    List<VeiculoModel> veiculos = new ArrayList<>();
+    List<Veiculo> veiculos = new ArrayList<>();
     
     public void cadastrarVeiculo(String marca, String modelo, String ano, double quilometragem, double valorDiaria) {
-        veiculos.add(new VeiculoModel(marca, modelo, ano, quilometragem, valorDiaria));
+        veiculos.add(new Veiculo(marca, modelo, ano, quilometragem, valorDiaria));
     }
     
     public void listarVeiculosDisponiveis() {
-        for (VeiculoModel veiculo : veiculos) {
+        for (Veiculo veiculo : veiculos) {
             System.out.println(veiculo);
         }
+    }
+    
+    public List<Veiculo> procurarVeiculoPelaMarca(String marca) {
+        return veiculos.stream()
+                .filter(v -> v.getMarca().equalsIgnoreCase(marca))
+                .toList();
+    }
+    
+    public List<Veiculo> procurarVeiculoPeloAno(String ano) {
+        return veiculos.stream()
+                .filter(v -> v.getAno().equalsIgnoreCase(ano))
+                .toList();
+    }
+    
+    public List<Veiculo> procurarVeiculoPeloModelo(String modelo) {
+        return veiculos.stream()
+                .filter(v -> v.getModelo().equalsIgnoreCase(modelo))
+                .toList();
     }
 }
