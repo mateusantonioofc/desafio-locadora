99e271cf696b0a153dc7aa907b7b465695b4171e
Mateus <mateusantonioofc@gmail.com>
2025-01-16 23:15:39 -0300

    update testes

--- src/main/java/org/locadora/LocadoraApplication.java
+++ src/main/java/org/locadora/LocadoraApplication.java
@@ -1,17 +1,11 @@
 package org.locadora;
 
-import java.util.ArrayList;
-import java.util.List;
-import org.locadora.sistema.SistemaLocadora;
-import org.locadora.veiculo.VeiculoModel;
+import org.locadora.services.GerarPlaca;
 
 public class LocadoraApplication {
 
     public static void main(String[] args) {
-        SistemaLocadora sistemaLocadora = new SistemaLocadora();
-        sistemaLocadora.cadastrarVeiculo("Honda", "HB20", "2020", 33, 200);
-        sistemaLocadora.cadastrarVeiculo("Nissan", "Corsa", "2013", 100.90, 100.40);
-        sistemaLocadora.cadastrarVeiculo("BMW", "XS", "2019", 0, 70.98);
-        sistemaLocadora.listarVeiculosDisponiveis();
+
+        System.out.println(GerarPlaca.gerarPlaca());
     }
 }
