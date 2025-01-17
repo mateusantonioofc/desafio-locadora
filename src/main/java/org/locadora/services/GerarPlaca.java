4a641944ba3edd8295fdc2884221405023ebac4e
Mateus <mateusantonioofc@gmail.com>
2025-01-16 23:15:22 -0300

    create generate plate

--- src/main/java/org/locadora/services/GerarPlaca.java
+++ src/main/java/org/locadora/services/GerarPlaca.java
@@ -1,5 +1,25 @@
 package org.locadora.services;
 
+import java.util.Random;
+
 public class GerarPlaca {
     
+    private static final String LETRAS = "ABCDEFGHIJKLOPQRSTUVWXYZ";
+    private static final Random random = new Random();
+    
+    public static String gerarPlaca() {
+        StringBuilder placa = new StringBuilder();
+        
+        for (int i = 0; i < 3; i++) {
+            int indice = random.nextInt(LETRAS.length());
+            placa.append(LETRAS.charAt(indice));
+        }
+        
+        for (int i = 0; i < 4; i++) {
+            int numero = random.nextInt(10);
+            placa.append(numero);
+        }
+        
+        return placa.toString();
+    }
 }
