a0dfa27e7f55f55f54f96b6da9bcb477ee65d250
Mateus <mateusantonioofc@gmail.com>
2025-01-18 02:55:02 -0300

    update testes

--- src/main/java/org/locadora/LocadoraApplication.java
+++ src/main/java/org/locadora/LocadoraApplication.java
@@ -6,25 +6,13 @@ public class LocadoraApplication {
 
     public static void main(String[] args) {
         SistemaLocadora sistemaLocadora = new SistemaLocadora();
-        sistemaLocadora.cadastrarVeiculo("Honda", "HB20", "2020", 33, 200);
-        sistemaLocadora.cadastrarVeiculo("Nissan", "Corsa", "2013", 100.90, 100.40);
-        sistemaLocadora.cadastrarVeiculo("BMW", "XS", "2019", 0, 70.98);
-
-        //System.out.println(sistemaLocadora.listarVeiculosDisponiveis());
-        //System.out.println(sistemaLocadora.listarTodosOsVeiculos());
-
-        SistemaLocadora teste = new SistemaLocadora();
-        teste.cadastrarVeiculo("MOKADO", "TESTE", "9999", 0, 0);
-        teste.procurarVeiculoPelaMarca("mokado");
-
-        //System.out.println(teste.procurarVeiculoPelaMarca("mokado"));
-        teste.procurarVeiculoPeloAno("9999");
-        teste.procurarVeiculoPeloModelo("Teste");
-
-        sistemaLocadora.cadastrarCliente("Joao");
-        sistemaLocadora.cadastrarCliente("Mateu");
-        sistemaLocadora.cadastrarCliente("Camis");
-        sistemaLocadora.cadastrarCliente("Jose");
-        System.out.println(sistemaLocadora.listarTodosOsClientes());
+        
+        for (int i = 0; i < 1000; i++) {
+            sistemaLocadora.cadastrarVeiculo("MARCA" + i, "MODELO" + i, "2000" + i, 0 + i, 0 + i);
+        }
+        
+        System.out.println(sistemaLocadora.listarTodosOsVeiculos());
+        
     }
 }
+
